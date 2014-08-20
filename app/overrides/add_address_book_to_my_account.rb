a0e3c05d150268654f4a87a6e847d23bcd1c091f
Nima Shariatian <nima@onevio.org>
2014-08-20 16:36:20 -0400

    updates: overrides and helpers to esnrue they work with spree 2.3 and rails 4

--- app/overrides/add_address_book_to_my_account.rb
+++ app/overrides/add_address_book_to_my_account.rb
@@ -1,7 +1,7 @@
 Deface::Override.new(
-  :virtual_path => "spree/users/show",
-  :name => "address_book_account_my_orders",
-  :insert_after => "[data-hook='account_my_orders'], #account_my_orders[data-hook]",
-  :partial => "spree/users/addresses",
-  :disabled => false
+    virtual_path: "spree/users/show",
+    name:         "address_book_account_my_orders",
+    insert_after: "[data-hook='account_my_orders'], #account_my_orders[data-hook]",
+    partial:      "spree/users/addresses",
+    disabled:     false
 )
