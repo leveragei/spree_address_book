92e60628cbb6cdc8d8393dac6c62235f7768a7d7
Nima Shariatian <nima@onevio.org>
2014-08-20 16:34:26 -0400

    adds: new spec_helper and rails_helper for rspec3 syntax

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,51 +1,26 @@
-# Configure Rails Environment
-ENV["RAILS_ENV"] ||= "test"
-
-require File.expand_path("../dummy/config/environment.rb",  __FILE__)
-
-require 'rspec/rails'
-require 'database_cleaner'
-require 'ffaker'
-
-# Requires supporting ruby files with custom matchers and macros, etc,
-# in spec/support/ and its subdirectories.
-Dir[File.join(File.dirname(__FILE__), "support/**/*.rb")].each {|f| require f }
-
-# Requires factories defined in spree_core
-require 'spree/core/testing_support/factories'
-require 'spree/core/testing_support/env'
-require 'spree/core/url_helpers'
-
 RSpec.configure do |config|
+
+  # == Mock Framework
+  #
+  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
+  #
+  # config.mock_with :mocha
+  # config.mock_with :flexmock
+  # config.mock_with :rr
   config.mock_with :rspec
+  config.color = true
+  config.formatter = :documentation
+
+  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
+  # Capybara javascript drivers require transactional fixtures set to false, and we use DatabaseCleaner
+  # to cleanup after each test instead.  Without transactional fixtures set to false the records created
+  # to setup a test will be unavailable to the browser, which runs under a separate server instance.
   config.use_transactional_fixtures = false
-  config.include Spree::Core::UrlHelpers
 
-  config.before(:each) do
-    if example.metadata[:js]
-      DatabaseCleaner.strategy = :truncation, {
-        :except => [
-          'spree_countries',
-          'spree_zones',
-          'spree_zone_members',
-          'spree_states',
-          'spree_roles'
-        ]
-      }
-    else
-      DatabaseCleaner.strategy = :transaction
-    end
-  end
+  config.infer_spec_type_from_file_location!
 
-  config.before(:each) do
-    DatabaseCleaner.start
-    reset_spree_preferences
-
-    # not sure exactly what is happening here, but i think it takes an iteration for the country data to load
-    Spree::Config[:default_country_id] = Spree::Country.find_by_iso3('USA').id if Spree::Country.count > 0
-  end
-
-  config.after(:each) do
-    DatabaseCleaner.clean
-  end
+  config.fail_fast = true
+  config.order = "random"
 end
